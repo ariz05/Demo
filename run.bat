20f0ef814764090ea569014a821402ec434b9e01
ariz05 <arizrizvi05@gmail.com>
2017-11-15 02:59:02 -0500

    commiting

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp C:/Users/arizr/workspace/Jenkins/bin;C:/Users/arizr/workspace/Jenkins/lib/* org.testng.TestNG testng.xml
+java -cp C:/Users/arizr/workspace/Jenkins/bin;C:/Users/arizr/workspace/Jenkins/lib/* org.testng.TestNG jenkins.xml
---
+++ src/JenkinsJobs/JenkinsShow.java
@@ -0,0 +1,73 @@
+package JenkinsJobs;
+
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+
+public class JenkinsShow {
+
+	public static WebDriver driver;
+ 
+	 @Test
+  
+		public static void demo() 
+			{
+	  
+			String exePath = "C:\\Users\\arizr\\Desktop\\DriversandJars\\chromedriver.exe";
+
+			System.setProperty("webdriver.chrome.driver", exePath);
+			WebDriver driver = new ChromeDriver();
+				
+			System.out.print("Welcome to Selenium World!!");
+		
+			/*DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+
+			capabilities.setCapability("marionette", true);
+	    
+			driver = new FirefoxDriver(capabilities);*/
+
+		    	driver.manage().window().maximize();
+
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+			driver.get("https://www.homeshop18.com/");
+	  
+	  
+  
+			}
+
+@Test
+  public static void demo2() {
+			String exePath = "C:\\Users\\arizr\\Desktop\\DriversandJars\\chromedriver.exe";
+
+			System.setProperty("webdriver.chrome.driver", exePath);
+			WebDriver driver = new ChromeDriver();
+			System.out.print("Welcome to Selenium World!!");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get("https://www.homeshop18.com/");
+			Assert.assertTrue(driver.getTitle().contains("Largest"));
+	  
+	  
+  }
+
+@AfterMethod
+public void EndTest()
+{
+	driver.quit();
+}
+}
+
+
+
